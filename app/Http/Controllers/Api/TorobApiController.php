5f97a96b3004ed6ee68869b13114c835fee75166
mohamadamin.zanguee <mohamadamin.zanguee@gmail.com>
2025-08-16 18:23:25 +0330

    fix torobv3

--- app/Http/Controllers/Api/TorobApiController.php
+++ app/Http/Controllers/Api/TorobApiController.php
@@ -46,28 +46,28 @@ class TorobApiController extends Controller
             $slugs[] = $slug;
         }
         $products = Product::whereIn('slug', $slugs)->get();
-        return $this->setResponse($products,1);
+        return $this->setResponse($products,1,count($products),1);
     }
 
     private function getProductsByIds($ids){
         $products = Product::whereIn('id', $ids)->get();
-        return $this->setResponse($products,1);
+        return $this->setResponse($products,1,count($products),1);
     }
 
     private function getProductsByPageNumber($request){
 
         $products = Product::orderBy($request['sort'] == 'date_added_desc' ? 'created_at' : 'updated_at', 'desc')
         ->paginate(100, ['*'], 'page', $request['page']);
-        return $this->setResponse($products->items(),$request['page']);
+        return $this->setResponse($products->items(),$request['page'],Product::query()->count(),(Product::query()->count()/100)+1);
     }
 
-    private function setResponse($products,$page_num){
+    private function setResponse($products,$page_num,$total,$max_pages){
 
         return [
             "api_version" => "torob_api_v3",
             "current_page" => $page_num,
-            "total" => (int)Product::count(),
-            "max_pages" => (int)(Product::count()/100)+1,
+            "total" => (int)$total,
+            "max_pages" => (int)$max_pages,
             "products" => TorobProductsResource::collection($products),
         ];
     }
